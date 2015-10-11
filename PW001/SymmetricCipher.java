227a84bfed90bc99d9bd2495e5501d02b82fc82a
Laura B <laubenmar@gmail.com>
2015-10-11 18:57:12 +0200

    Funcion padding añadida

--- PW001/SymmetricCipher.java
+++ PW001/SymmetricCipher.java
@@ -57,27 +57,10 @@ public class SymmetricCipher {
 		}
 			//Check arraycopy
 
-			for (int i =0;i<totalBlocks; i++)
-			{
-
-
-
-
-
-					if (i==0)
-					{
-						//XOR con IV Bloque 1
-					}
-					else
-					{
-						// XOR bloque i con bloqueYaCifrado i-1
-
-					}
-			}
-
-
+			
 			// Generate the plaintext with padding
 
+			
  //Aquí hay un byte con el texto con padding
 			Vector<byte> vTextPad[] = new Vector<byte>();
 			vTextPad = division(textPadd, totalBlocks);
@@ -150,5 +133,61 @@ public class SymmetricCipher {
 		}
 		return resultado;
 	}
+	
+	/*      	>>> Metodo addPadding <<<
+	 * El bloque de entrada tendrá entre 0 y 15 bytes
+	 * 
+	 * El numero de bytes que faltan serán entre 1 y 16
+	 * Se utiliza el formato #PKCS5 que consiste en:
+	 * Rellenar los huecos de bytes con el valor numerico
+	 * del numero de bytes necesarios
+	 * Si faltan 3 bytes, se rellenan 3 bytes con 0x03
+	 * Si faltan 2 bytes, se rellenan 2 bytes con 0x02
+	 * Si faltan 1 bytes, se rellenan 1 bytes con 0x01
+	 * 
+	 * Devuelve un array de bytes ya con su padding correspondiente
+	 *  
+	 * */
+	public byte[] addPadding(byte[] input) throws Exception
+	{	
+		
+		byte[] blockWithPadding = null;
+		int inputLength;
+		
+		//Si el array está vacío la longitud es 0
+		if(input == null)
+		{
+			inputLength= 0;
+		}
+		else if(input.length >= 16)
+		{
+			//Si tiene 16 bytes o mas no calculamos padding 
+			//Para el padding de los bloques enteros le pasamos null
+			return null;
+		}
+		else
+		{
+			inputLength = input.length;
+		}
+		
+		int missingBytes = 16 - inputLength;
+		//Creamos un array de bytes del tamaño de los bytes que faltan
+		byte[] padding = new byte[missingBytes];
+		//Rellenamos el array con el valor del numero de bytes que faltan
+		Arrays.fill(padding, (byte)missingBytes);		
+		System.out.println(Arrays.toString(padding));
+	    
+		//Concatenamos los dos arrays resultantes 
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+	  		
+		outputStream.write(input);
+		outputStream.write(padding);
+	  	
+		//Convertimos a byte[] de nuevo
+		blockWithPadding = outputStream.toByteArray( );
+		
+		return blockWithPadding;
+	}
+
 
 }
