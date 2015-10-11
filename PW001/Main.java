8139cc10f96f695abcf0077b8dcff182ddd9734f
Rafael León <fucho@me.com>
2015-10-11 18:22:38 +0200

    esqueleto para el main

---
+++ PW001/Main.java
@@ -0,0 +1,16 @@
+/*
+Class to check the encryption and decryption functions
+Laura Benito Martin 284695
+Rafael Leon Miranda 275593
+*/
+
+import java.io.*;
+
+public class Main {
+
+
+  public static  void main (String [ ] args) {
+
+}
+
+}
